55e157c2249fd6a22689e9ab092188c3195da1b2
admin-00007 <140091643+ADMIN-00007@users.noreply.github.com>
2023-08-31 15:50:59 +0200

    first commit

---
+++ Btc/BtcDashBoard/Form1.cs
@@ -0,0 +1,15 @@
+namespace BtcDashBoard
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://forms.office.com/Pages/ResponsePage.aspx?id=Uxo8h1TYokeM71lH3Fj_sdQBH5zizjBImbSQ_pb3sepURExWQkY2VzlVTlhWNFZERUxQVzZGMTA0RS4u");
+        }
+    }
+}
